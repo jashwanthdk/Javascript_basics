92e656ba97f6e6ec67c909ba00ac774c7a2ad6cb
jashwanthdk <56800282+jashwanthdk@users.noreply.github.com>
2020-06-13 13:19:06 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,25 @@
+document.write("<h2>Hello World<h2>");
+document.write("<h1>Heading 3</h1>");
+document.write("<h1>Heading 5</h1>");
+
+var num = 16;
+
+num = "Tanmay"
+// alert(num);
+
+// javascript is dynamically executed program.
+// It runs the code line by line
+// In this case its alerting num="Tanmay"
+
+var a = 3;
+var b = 5;
+var c = 2;
+var result = a+b+c;
+document.write(result);
+
+var result1 = a + b + "Tanmay" + a + b;
+document.write(result1);
+//before the string the operators and the operands.
+// and after the string the operators are concatinated.
+
+//operator precidence
